ade0fe820e9c3d5b24ce67f88f2f58cf9f4fdb97
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-07 22:25:51 -0300

    Setup typescript and clean up boilerplate

---
+++ pages/_app.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Developer DAO</title>
+        <meta
+          name="description"
+          content="Frontend interface for Developer DAO - Devs for Revolution."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="apple-mobile-web-app-title" content="Developer DAO" />
+        <meta name="application-name" content="Developer DAO" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
