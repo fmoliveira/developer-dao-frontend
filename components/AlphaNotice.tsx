8a4627d2638b482e6fd5c5763f9edcb8b827bc78
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-25 01:00:35 -0300

    Update notice

--- components/AlphaNotice.tsx
+++ components/AlphaNotice.tsx
@@ -3,8 +3,8 @@ export function AlphaNotice() {
 		<div className="-mx-4 -mt-4 mb-8 p-4 bg-gray-900 border-b-2 border-green-200 flex gap-4">
 			<span className="px-1">👻</span>
 			<div>
-				Please bear on me while I adjust the dark theme colours.{" "}
-				<span className="px-1">🙄</span>
+				Please bear on me while I adjust the dark theme colours and the overall
+				design. <span className="px-1">🙄</span>
 			</div>
 		</div>
 	);
