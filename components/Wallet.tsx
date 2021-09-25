357becddc17ba4668c9f995d02836db3b3ba8dd1
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-25 00:59:58 -0300

    Touch up wallet colours

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -24,10 +24,10 @@ export default function Wallet({
 	}
 
 	return (
-		<div className="h-16 text-center">
+		<div className="h-16 mb-4 text-center">
 			{!walletInstalled && (
 				<a
-					className="inline-block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+					className="inline-block bg-red-700 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
 					href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn/related"
 					target="_blank"
 					rel="noopener noreferrer"
@@ -37,7 +37,7 @@ export default function Wallet({
 			)}
 			{walletInstalled && !walletConnected && (
 				<button
-					className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+					className="bg-yellow-600 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded"
 					onClick={connectWallet}
 				>
 					Connect MetaMask
@@ -46,19 +46,19 @@ export default function Wallet({
 			{walletConnected && (
 				<div>
 					<div>
-						<span className="mr-2 inline-block p-1 bg-green-500 rounded-md" />
+						<span className="mr-2 inline-block p-1 bg-green-400 rounded-md" />
 						Wallet Connected
 					</div>
 					<div
 						className={classNames(
 							"font-bold",
-							isMainNet ? "text-green-500" : "text-red-500",
+							isMainNet ? "text-green-400" : "text-red-400",
 						)}
 					>
 						Network: <span className="capitalize">{networkName}</span>
 					</div>
 					{!isMainNet && (
-						<div className="text-red-500 font-bold">
+						<div className="text-red-400 font-bold">
 							Please switch to Mainnet
 						</div>
 					)}
